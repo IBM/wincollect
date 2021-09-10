76b149af59ac1e82d6fdc80dc851331efc62f530
joshryan-wc <josh.ryan@ca.ibm.com>
2021-09-10 14:55:03 -0300

    Create InstallWC10Agent.ps1

---
+++ WinCollect10/AgentInstallation/InstallWC10Agent.ps1
@@ -0,0 +1,12 @@
+﻿
+$file = '\\yournetworkshare\wincollect\10\wincollect-10.0.1-276.x64.msi'
+$computerName = 'yourtargetendpoint'
+
+
+$session = New-PSSession -ComputerName $computerName
+Copy-Item -Path $file -ToSession $session -Destination c:\windows\temp\wincollect-10.0.1-276.x64.msi
+
+Invoke-Command -Session $session -ScriptBlock {
+    Start-Process msiexec.exe -Wait -ArgumentList '/i C:\Windows\Temp\wincollect-10.0.1-276.x64.msi QUICK_INSTALL="yes" WC_DEST="172.18.233.102"'
+}
+Remove-PSSession $session
