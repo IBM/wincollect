c6a9f01e94df23c67dd842c4d736a6cc05cd0316
Jon-IBM <52573525+Jon-IBM@users.noreply.github.com>
2023-06-06 11:31:01 -0300

    Update InstallWC10Agent.ps1

--- WinCollect10/Powershell Scripts/AgentInstallation/InstallWC10Agent.ps1
+++ WinCollect10/Powershell Scripts/AgentInstallation/InstallWC10Agent.ps1
@@ -7,6 +7,6 @@ $session = New-PSSession -ComputerName $computerName
 Copy-Item -Path $file -ToSession $session -Destination c:\windows\temp\wincollect-10.0.1-276.x64.msi
 
 Invoke-Command -Session $session -ScriptBlock {
-    Start-Process msiexec.exe -Wait -ArgumentList '/i C:\Windows\Temp\wincollect-10.0.1-276.x64.msi QUICK_INSTALL="yes" WC_DEST="172.18.233.102"'
+    Start-Process msiexec.exe -Wait -ArgumentList '/i C:\Windows\Temp\wincollect-10.0.1-276.x64.msi QUICK_INSTALL="yes" WC_DEST="172.18.233.102" ADMIN_GROUP="true"'
 }
 Remove-PSSession $session
