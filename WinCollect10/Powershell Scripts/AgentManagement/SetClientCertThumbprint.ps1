cdc532c8473395f4f6ead0e136330df1b038d469
Will <93391385+WillL-IBM@users.noreply.github.com>
2023-06-13 13:37:19 -0300

    Update SetClientCertThumbprint.ps1
    Signed-off-by: Will Leonard <will.leonard@ibm.com>

--- WinCollect10/Powershell Scripts/AgentManagement/SetClientCertThumbprint.ps1
+++ WinCollect10/Powershell Scripts/AgentManagement/SetClientCertThumbprint.ps1
@@ -1,8 +1,13 @@
-#Checks the "My" (or "Personal") certificate store of the local machine for a certificate issued to this machine.
-#Make sure to replace <hostname> with your machine's hostname
+#Checks the "My" (or "Personal") certificate store of the local machine for a certificate issued to this machine
+param 
+(
+    [string]$hostname = $env:computername
+)
+
+Write-Output "Searching for certificate issued to '$hostname' in LocalMachine certificate store 'Personal'"
 [Environment]::SetEnvironmentVariable(
         'WC_CLIENT_CERT_THUMBPRINT', 
         (Get-ChildItem -Path cert:\LocalMachine\My | 
-        Where-Object Subject -Like "*CN=<hostname>*" | 
+        Where-Object Subject -Like "*CN=$hostname*" | 
         Select-Object -First 1).Thumbprint, 
         'Machine')
